65221a91b72908b703768020c1ac1d1a9b19379d
Nathalie <info@nathalieborst.nl>
2018-08-01 09:25:11 +0200

    updated app.php

--- app.php
+++ app.php
@@ -17,7 +17,7 @@ echo <<<HTML
     <head>
         <title>Hello World</title>
         <script type="text/javascript">
-            var Speakap = { appId: "2900ee6345000aa8", signedRequest: "$encSignedReq" };
+            var Speakap = { appId: "2900ee6345000aa8", signedRequest: "10bad4d0182940145fb2231c774c1817b45460c3d3f244b09e3baacd35306ae9" };
         </script>
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/speakap.js"></script>
@@ -28,3 +28,18 @@ echo <<<HTML
     </body>
 </html>
 HTML;
+
+// <html>
+//     <head>
+//         <title>Hello World</title>
+//         <script type="text/javascript">
+//             var Speakap = { appId: "...", signedRequest: "..." };
+//         </script>
+//         <script type="text/javascript" src="js/jquery.min.js"></script>
+//         <script type="text/javascript" src="js/speakap.js"></script>
+//     </head>
+//
+//     <body>
+//         <p>Hello Speakap user!</p>
+//     </body>
+// </html>
