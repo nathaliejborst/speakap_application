c61bc6fb43e0479b44af5ca4e908688a5b41e11b
Nathalie <info@nathalieborst.nl>
2018-08-01 09:26:23 +0200

    updated app.php

--- app.php
+++ app.php
@@ -1,18 +1,3 @@
-<?php
-
-// Assume auto-load is in place for the Speakap\SDK namespace
-
-use Speakap\SDK as SpeakapSDK;
-
-$signedRequest = new SpeakapSDK\SignedRequest('2900ee6345000aa8', '10bad4d0182940145fb2231c774c1817b45460c3d3f244b09e3baacd35306ae9');
-
-if (!$validator->validateSignature($_POST)) {
-    die('Invalid signature');
-}
-
-$encSignedReq = $signedRequest->getSignedRequest($_POST);
-
-echo <<<HTML
 <html>
     <head>
         <title>Hello World</title>
@@ -27,19 +12,3 @@ echo <<<HTML
         <p>Hello Speakap user!</p>
     </body>
 </html>
-HTML;
-
-// <html>
-//     <head>
-//         <title>Hello World</title>
-//         <script type="text/javascript">
-//             var Speakap = { appId: "...", signedRequest: "..." };
-//         </script>
-//         <script type="text/javascript" src="js/jquery.min.js"></script>
-//         <script type="text/javascript" src="js/speakap.js"></script>
-//     </head>
-//
-//     <body>
-//         <p>Hello Speakap user!</p>
-//     </body>
-// </html>
