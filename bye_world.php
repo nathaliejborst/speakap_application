4560cbb2f0d4aa087d8ebcc54691cde34979fe33
Nathalie <info@nathalieborst.nl>
2018-08-01 11:46:52 +0200

    updated bye_world.php

--- bye_world.php
+++ bye_world.php
@@ -1,9 +1,14 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+
 require 'vendor/autoload.php';
+
 $signedRequest = new \Speakap\SDK\SignedRequest('2901771f37000d34', 'af37fd105d5f047a1d4e60c6492991b5f6d526de51efdbc9efe927aa0665d7ba');
-echo 'Bla';
+
+
 if (!$signedRequest->validateSignature($_POST)) {
     die('Invalid signature');
 }
+
+echo 'Bye world ...';
