ae132771ef79d40ff8ee7199c6cd003c29faffd0
Nathalie <info@nathalieborst.nl>
2018-08-01 09:07:17 +0200

    updated app.php

--- hello_app.php
+++ hello_app.php
@@ -4,13 +4,6 @@
     </head>
 
     <body>
-        <svg version="1.1"
-     baseProfile="full"
-     width="300" height="200"
-     xmlns="http://www.w3.org/2000/svg">
-  <rect width="100%" height="100%" fill="black" />
-  <circle cx="150" cy="100" r="90" fill="blue" />
-</svg>
         <p>Hello Speakap user!</p>
     </body>
 </html>
