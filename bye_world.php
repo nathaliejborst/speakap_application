acfe289d495e4e9052e4d06af34ff977d194bd41
Nathalie <info@nathalieborst.nl>
2018-08-01 10:19:52 +0200

    added bye_world.php

---
+++ bye_world.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Bye World</title>
+        <script type="text/javascript">
+            var Speakap = { appId: "2900fd4ddb000f04 ", signedRequest: "b10cbcc2d6ffda09b59a7a3595c448f1855b1b83f0b09c30724ebb2254cd067d" };
+        </script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/speakap.js"></script>
+    </head>
+
+    <body>
+        <p>Hello Speakap user!</p>
+    </body>
+</html>
